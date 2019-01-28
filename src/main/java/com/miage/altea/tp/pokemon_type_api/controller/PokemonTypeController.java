d0b924196150b49affd5cc3d991d31a089e93095
Brilhault <amelie.brilhault@worldline.com>
2019-01-28 11:52:17 +0100

    first commit - up to part 8.5.1

---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/controller/PokemonTypeController.java
@@ -0,0 +1,35 @@
+package com.miage.altea.tp.pokemon_type_api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
+import com.miage.altea.tp.pokemon_type_api.service.PokemonTypeService;
+
+@RestController
+@RequestMapping("/pokemon-types")
+public class PokemonTypeController {
+
+	private PokemonTypeService pokemonTypeService;
+
+	@Autowired
+	public PokemonTypeController(PokemonTypeService service) {
+		this.pokemonTypeService = service;
+	}
+
+	@GetMapping("/{id}")
+	PokemonType getPokemonTypeFromId(@PathVariable("id") int id){
+		return pokemonTypeService.getPokemonType(id);
+	}
+
+	@GetMapping("/")
+	public List<PokemonType> getAllPokemonTypes() {
+		return pokemonTypeService.getAllPokemonTypes();
+	}
+
+}
