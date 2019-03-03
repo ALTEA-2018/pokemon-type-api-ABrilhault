970c493da02fbca1cc9ca4c0bc181e0f486f186e
Brilhault <amelie.brilhault@worldline.com>
2019-03-03 16:01:10 +0100

    up to part 2.4 : Translation on list of all pokemon types

--- src/main/java/com/miage/altea/tp/pokemon_type_api/service/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/service/PokemonTypeServiceImpl.java
@@ -1,27 +1,48 @@
 package com.miage.altea.tp.pokemon_type_api.service;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
 import com.miage.altea.tp.pokemon_type_api.repository.PokemonTypeRepository;
+import com.miage.altea.tp.pokemon_type_api.repository.TranslationRepository;
 
 @Service
 public class PokemonTypeServiceImpl implements PokemonTypeService {
 
 	public PokemonTypeRepository pokemonTypeRepository;
+	public TranslationRepository translationRepository;
 
 	@Autowired
-	public PokemonTypeServiceImpl(PokemonTypeRepository repository){
+	public PokemonTypeServiceImpl(PokemonTypeRepository repository, TranslationRepository tr){
 		this.pokemonTypeRepository = repository;
+		this.translationRepository = tr;
 	}
 
+	public PokemonTypeServiceImpl() {
+	}
+
+	public PokemonTypeServiceImpl(PokemonTypeRepository pokemonTypeRepository) {
+		this.pokemonTypeRepository = pokemonTypeRepository;
+	}
 
 	@Override
 	public PokemonType getPokemonType(int id) {
-		return pokemonTypeRepository.findPokemonTypeById(id);
+		if (translationRepository == null ) {
+			PokemonType pt = new PokemonType();
+			pt.setId(id);
+			return pt;
+		} else {
+			Locale locale = LocaleContextHolder.getLocale();
+			PokemonType pt = pokemonTypeRepository.findPokemonTypeById(id);
+			pt.setName(translationRepository.getPokemonName(id, locale));
+			return pt;
+		}
 	}
 
 	@Override
@@ -31,8 +52,24 @@ public class PokemonTypeServiceImpl implements PokemonTypeService {
 
 	@Override
 	public List<PokemonType> getAllPokemonTypes(){
-		return pokemonTypeRepository.findAllPokemonType();
+		Locale locale = LocaleContextHolder.getLocale();
+		return pokemonTypeRepository.findAllPokemonType().stream().map(t -> {t.setName(translationRepository.getPokemonName(t.getId(), locale)); return t;}).collect(
+				Collectors.toList());
 	}
 
+	public PokemonTypeRepository getPokemonTypeRepository() {
+		return pokemonTypeRepository;
+	}
 
+	public void setPokemonTypeRepository(PokemonTypeRepository pokemonTypeRepository) {
+		this.pokemonTypeRepository = pokemonTypeRepository;
+	}
+
+	public TranslationRepository getTranslationRepository() {
+		return translationRepository;
+	}
+
+	public void setTranslationRepository(TranslationRepository translationRepository) {
+		this.translationRepository = translationRepository;
+	}
 }
